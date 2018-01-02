79b8a0ce7ced8df93280c30211a313c680e43ead
shawngmc <shawngmc@gmail.com>
2018-01-01 20:37:55 -0500

    Update dash-listen.py

--- dash-listen.py
+++ dash-listen.py
@@ -8,13 +8,20 @@ import os
 import requests
 
 DASH_MAC_ADDRESS=os.environ.get('DASH_MAC_ADDRESS')
-URL_CALLBACK=os.environ.get('URL_CALLBACK')
+HA_API_BASE=os.environ.get('HA_API_BASE')
+HA_SERVICE=os.environ.get('HA_SERVICE')
+HA_ENTITY_ID=os.environ.get('HA_ENTITY_ID')
+HA_PASSWORD=os.environ.get('HA_PASSWORD')
+
+URL_CALLBACK=HA_API_BASE + "/services/" + HA_SERVICE.replace('.', '/')
+CALL_BODY='{"entity_id":"' + HA_ENTITY_ID + '"}'
+HEADERS={'X-HA-access': HA_PASSWORD}
 
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
     if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
       if pkt[ARP].hwsrc == DASH_MAC_ADDRESS:
-        requests.get(URL_CALLBACK)
+        requests.get(URL_CALLBACK, headers=HEADERS, data=CALL_BODY)
       else:
         print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 
